1f2894b0297111b312f4b35c3f81446fb46b02ce
lcesbron <190340561+lcesbron@users.noreply.github.com>
2026-03-04 12:00:21 +0100

    refacto: ex01

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,27 +1,28 @@
 #include "RPN.hpp"
 #include <exception>
 #include <iostream>
+#include <ostream>
 
 int	main(int ac, char **av)
 {
-	RPN	rpn(av[1]);
-	int	toPrint;
-
-	if (ac != 2)
-	{
-		std::cout << "./RPN \"expression\"" << std::endl;
-		return (1);
-	}
-
 	try
 	{
+		RPN	rpn(av[1]);
+		int	toPrint;
+
+		if (ac != 2)
+		{
+			std::cout << "./RPN \"expression\"" << std::endl;
+			return (1);
+		}
+
 		toPrint = rpn.computeExpression();
+
+		std::cout << toPrint << std::endl;
 	}
 	catch (std::exception const& e)
 	{
 		std::cout << e.what() << std::endl;
 		return (1);
 	}
-
-	std::cout << toPrint << std::endl;
 }
